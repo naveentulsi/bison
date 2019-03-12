1940853cb12e15aeb524b374327b9ecc3e12e8a5
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-03-12 22:20:03 +0530

    added post method for test

--- src/main/java/one/bison/controller/BisonController.java
+++ src/main/java/one/bison/controller/BisonController.java
@@ -4,6 +4,8 @@
 package one.bison.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,7 +17,12 @@ public class BisonController {
 	
 	@GetMapping
 	public String test() {
-		return "Service is Up";
+		return "You are have requested a GET method. Thanks!";
+	}
+	
+	@PostMapping
+	public String testPost(@RequestBody String body) {
+		return String.format("You have reached a POST Method with body %s . Thanks!", body);
 	}
 
 }
